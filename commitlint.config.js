6ffc680194efe51d07d6124eed53351ba79c6028
Dmytro Misik <d.misik@draftkings.com>
2024-09-09 13:54:13 +0300

    feat: update husky to allow commit message length to be 1000 symbols

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,7 @@
+// commitlint.config.js
 module.exports = {
     extends: ['@commitlint/config-conventional'],
+    rules: {
+        'body-max-line-length': [2, 'always', 1000], // Sets the max body line length to 1000 characters
+    },
 };

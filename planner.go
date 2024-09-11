1b1b244529273ed48bd30d5e18e0baf768654d48
Dmytro Misik <d.misik@draftkings.com>
2024-09-11 16:06:48 +0300

    feat: update scheduler interface & cover with more tests

--- planner.go
+++ planner.go
@@ -6,16 +6,23 @@ import (
 )
 
 type planner struct {
-	threads uint
-	jobs    chan JobContext
-	errs    chan error
+	threads     uint
+	jobs        chan *JobContext
+	errs        chan error
+	subscribers []PlannerSubscriber
 }
 
-func (p *planner) Errs() <-chan error {
-	return p.errs
+func (p *planner) Subscribe(subscriber PlannerSubscriber) {
+	p.subscribers = append(p.subscribers, subscriber)
 }
 
-func (p *planner) Plan(ctx JobContext) error {
+func (p *planner) Plan(ctx *JobContext) (res error) {
+	defer func() {
+		if err := recover(); err != nil {
+			res = err.(error)
+		}
+	}()
+
 	select {
 	case p.jobs <- ctx:
 		return nil
@@ -26,7 +33,6 @@ func (p *planner) Plan(ctx JobContext) error {
 
 func (p *planner) Stop() error {
 	close(p.jobs)
-	close(p.errs)
 
 	return nil
 }
@@ -36,7 +42,22 @@ func (p *planner) Start(ctx context.Context) error {
 		go p.executor(ctx)
 	}
 
+	go p.errsListener(ctx)
+
 	return nil
+}
+
+func (p *planner) errsListener(ctx context.Context) {
+	for {
+		select {
+		case err := <-p.errs:
+			for _, s := range p.subscribers {
+				go s.OnError(err)
+			}
+		case <-ctx.Done():
+			return
+		}
+	}
 }
 
 func (p *planner) executor(ctx context.Context) {
@@ -45,6 +66,8 @@ func (p *planner) executor(ctx context.Context) {
 		case <-ctx.Done():
 			return
 		case job := <-p.jobs:
+			job.ExecutionStatus = JobExecutionStatusPlanned
+
 			if time.Until(job.PlannedAt) > 0 {
 				select {
 				case <-ctx.Done():
@@ -53,21 +76,29 @@ func (p *planner) executor(ctx context.Context) {
 				}
 			}
 
-			job.ExecutedAt = time.Now()
+			job.StartedAt = time.Now()
+			job.ExecutionStatus = JobExecutionStatusExecuting
 			err := job.Job.Execute(job)
 			if err != nil {
 				p.errs <- err
+				job.ExecutionStatus = JobExecutionStatusFailed
+			} else {
+				job.ExecutionStatus = JobExecutionStatusExecuted
 			}
 
-			job.executed <- struct{}{}
+			job.ExecutedAt = time.Now()
+			for _, s := range p.subscribers {
+				go s.OnJobExecuted(job)
+			}
 		}
 	}
 }
 
 func newPlanner(threads uint) Planner {
 	return &planner{
-		jobs:    make(chan JobContext, threads),
-		threads: threads,
-		errs:    make(chan error, 1),
+		jobs:        make(chan *JobContext, threads),
+		threads:     threads,
+		errs:        make(chan error),
+		subscribers: make([]PlannerSubscriber, 0),
 	}
 }

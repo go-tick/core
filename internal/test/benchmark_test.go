c5d49aa0ec6b6a926862a5efe2e70bbec17e0b88
Dmytro Misik <d.misik@draftkings.com>
2024-09-15 21:55:40 +0300

    test: implement benchmark test for scheduler

---
+++ internal/test/benchmark_test.go
@@ -0,0 +1,53 @@
+package test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/misikdmytro/gotick"
+	"github.com/stretchr/testify/require"
+)
+
+type benchmarkJob struct {
+	id   string
+	done chan any
+}
+
+func (b *benchmarkJob) Execute(*gotick.JobExecutionContext) {
+	close(b.done)
+}
+
+func (b *benchmarkJob) ID() string {
+	return b.id
+}
+
+var _ gotick.Job = (*benchmarkJob)(nil)
+
+func newBenchmarkJob(id string) *benchmarkJob {
+	return &benchmarkJob{id, make(chan any)}
+}
+
+func BenchmarkJobBetweenScheduleAndExecution(b *testing.B) {
+	scheduler := gotick.NewScheduler(gotick.DefaultSchedulerConfig(
+		gotick.WithIdlePollingInterval(0),
+	))
+	err := scheduler.Start(context.Background())
+	require.NoError(b, err)
+	defer scheduler.Stop()
+
+	b.ResetTimer()
+	for range b.N {
+		job := newBenchmarkJob(uuid.NewString())
+		schedule := gotick.NewCalendarSchedule(time.Now())
+
+		err = scheduler.RegisterJob(job)
+		require.NoError(b, err)
+
+		_, err = scheduler.ScheduleJob(context.Background(), job.ID(), schedule)
+		require.NoError(b, err)
+
+		<-job.done
+	}
+}

3571430ab4c99e0f3b3232e14cdad6c002ff2cb3
Dmytro Misik <d.misik@draftkings.com>
2024-09-15 23:33:54 +0300

    docs: update example

--- examples/simple-console/main.go
+++ examples/simple-console/main.go
@@ -161,7 +161,7 @@ func main() {
 	panicOnErr(err)
 
 	for i := range numOfJobs {
-		sch := gotick.NewCalendarWithMaxDelay(time.Now().Add(time.Duration(i+1)*time.Second), 5*time.Second)
+		sch := gotick.NewCalendarScheduleWithMaxDelay(time.Now().Add(time.Duration(i+1)*time.Second), 5*time.Second)
 
 		scheduler.ScheduleJob(ctx, job.ID(), sch)
 		panicOnErr(err)
